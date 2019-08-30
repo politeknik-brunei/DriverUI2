11b40ae6e4c7d908dab3611b5d38439fbeb0a7b5
Syaza <nws07_01094428@pb.edu.bn>
2019-08-30 23:35:44 +0800

    HOMEPAGE (PULL TIA SMUA)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,34 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  public appPages =[
+    
+    {
+      title: "Profile",
+      // url: "/userprofile-page",
+      // icon:"user"
+    },
+    
+    {
+      title: "Home",
+      url: "/homepage",
+      // icon: "home"
+    },
+
+    {
+      title: "Contact Us",
+      // url: "/customercare",
+      // icon: "services"
+    },
+
+    {
+      title: "Logout",
+      // url:
+      // icon: "logout"
+    }
+  ];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
